92c4d40a95bcf2f34200628d3932d86495d44607
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2021-11-25 14:08:40 +0100

    rename kitty terminal title to "Kitty Terminal"

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -6,7 +6,7 @@ return {
 	-- The default applications that we will use in keybindings and widgets
 	default = {
 		-- Default terminal emulator
-		terminal = 'env kitty -T=$USER@$(hostname)',
+		terminal = 'env kitty -T="Kitty Terminal"',
 		-- Default web browser
 		web_browser = 'env brave-browser',
 		-- Default text editor
